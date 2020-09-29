f7f9d4349fe7d2f1c71e71e641378528de3296a3
Peyton Kuhlers <p.kuhl1711@gmail.com>
2020-09-29 15:51:50 -0400

    Updated scratch file to produce asset

--- scratch.R
+++ scratch.R
@@ -16,6 +16,15 @@ beer %>%
                values_ptypes = list(year = numeric(),barrels=numeric())) -> beerTidy
 beerTidy$year <- as.numeric(beerTidy$year)
 
+png("assets/barrels_by_state.png")
 beerTidy %>%
-  ggplot(aes(x = year, y = barrels, color = STATE)) + geom_line()
+  ggplot(aes(x = year, y = barrels, color = STATE)) + 
+  geom_line(show.legend = F) +
+  coord_cartesian(xlim = c(2008,2019)) + 
+  scale_x_continuous(name = "Year", 
+                     breaks = seq(2008,2019,2),
+                     limits = c(2008,2019)) +
+  labs(y= "Barrels",
+       title = "Barrels of Beer Prouduced from 2008 - 2009")
+dev.off()
          
